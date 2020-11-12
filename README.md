82ae75e35c72dd0c3adece258f76d5fd3e823b8e
Thomas An <tommyan97@msn.com>
2020-11-12 11:57:08 -0800

    added cuisine and service type seeds

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chef-app-backend
+
+Use 'npm run seed' to seed database
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -8,17 +8,51 @@ mongoose.connect(
 );
 
 const cuisineSeed = [
-    {name: "Italian"}, 
-    {name: "Thai"}
+    {name: "American"},
+    {name: "Cajun"},
+    {name: "Cantonese"},
+    {name: "Chinese"},
+    {name: "Danish"},
+    {name: "Ethiopian"},
+    {name: "French"},
+    {name: "Filipino"},
+    {name: "Greek"},
+    {name: "Indian"},
+    {name: "Italian"},
+    {name: "Japanese"},
+    {name: "Keto"},
+    {name: "Korean"},
+    {name: "Mediterranean"},
+    {name: "Mexican"},
+    {name: "Thai"},
+    {name: "Vegan"},
+    {name: "Vegetarian"},
+    {name: "Vietnamese"}
+];
+const serviceTypeSeed = [
+    {name: "Hourly"}, 
+    {name: "Per Meal"},
+    {name: "Per Person"},
 ]
-// const chefSeed = []
-// const serviceTypeSeed = []
+
+
 // const clientSeed = []
 
-db.Cuisine.remove({})
+db.Cuisine.deleteMany({})
     .then(() => db.Cuisine.insertMany(cuisineSeed))
     .then(data => {
-        console.log(data.result.n + " records inserted!");
+        console.log(data.length + " cuisine records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+
+db.ServiceType.deleteMany({})
+    .then(() => db.ServiceType.insertMany(serviceTypeSeed))
+    .then(data => {
+        console.log(data.length + " serviceType records inserted!");
         process.exit(0);
     })
     .catch(err => {
