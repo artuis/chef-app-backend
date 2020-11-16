0fa9ce2aff6c263bd718de39464eb6de7dc39ae0
Thomas An <tommyan97@msn.com>
2020-11-15 23:10:35 -0800

    added cors function for localhost

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const routes = require("./routes");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+app.use(cors(
+    {
+        origin : "http://localhost:3000",
+        optionsSuccessStatus: 200
+    }
+));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
